3540b75e42855f7116c111a831a2e70375be9fa5
mirroraautospa <mirroraautospa@gmail.com>
2026-07-30 11:24:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Mirrora Website Loaded");
 
 });
+const form = document.getElementById("booking-form");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "mirrora_service",
+        "template_vbh0nba",
+        this
+    )
+
+    .then(function () {
+
+        alert("Thank you! Your appointment request has been sent.");
+
+        form.reset();
+
+    })
+
+    .catch(function (error) {
+
+        alert("Something went wrong. Please try again.");
+
+        console.log(error);
+
+    });
+
+});
